3c766b79758e8381dbcbfcad25cf926a0302cfe5
Ubuntu <ubuntu@ip-172-31-23-1.us-east-2.compute.internal>
2022-10-17 11:06:05 +0000

    Updated detections module

--- detections/views.py
+++ detections/views.py
@@ -41,79 +41,88 @@ class CariesDetectionView(CreateAPIView):
             model = project.version(2).model
             worker = os.getpid()
             denture_images = serializer.validated_data["denture_images"]
-            try:
-                for indx, imagebytes in enumerate(denture_images):
-                    filename = f"/home/ubuntu/dentasis/detections/detect/{worker}/{indx}.png"
-                    os.makedirs(os.path.dirname(filename), exist_ok=True)
-                    with open(filename, "wb") as f:
-                        f.write(base64.b64decode(imagebytes))
-                        f.close()
+            #try:
+            for indx, imagebytes in enumerate(denture_images):
+                filename = f"/home/ubuntu/dentasis/detections/detect/{worker}/{indx}.jpeg"
+                os.makedirs(os.path.dirname(filename), exist_ok=True)
+                PIL_image = Image.open(io.BytesIO(base64.b64decode(imagebytes)))
+                PIL_image.save(filename, quality=70, optimize=True)
+                print("created: ", filename)
 
-                    print(filename)
-                    prediction = model.predict(filename)
-                    results = prediction.json()["predictions"]
-                    confs = []
-                    image = cv2.imread(filename)
-                    stroke_color = (83, 12, 145)
-                    stroke = 1
-                    for result in results:
-                        conf = float(result["confidence"]) * 100
-                        conf = f"{conf:.2f}%"
-                        confs.append(conf)
+                print("starting predictions...")
+                prediction = model.predict(filename)
+                results = prediction.json()["predictions"]
+                print("processing results...")
 
-                        width = result["width"]
-                        height = result["height"]
-                        x = result["x"]
-                        y = result["y"]
+                confs = []
+                image = cv2.imread(filename)
+                stroke_color = (83, 12, 145)
+                stroke = 1
+                for result in results:
+                    conf = float(result["confidence"])
+                    confs.append(f"{conf:.2f}")
+                    conf *= 100
+                    conf = f"{conf:.2f}%"
 
-                        cv2.rectangle(
-                                image,
-                                (int(x - width / 2), int(y + height / 2)),
-                                (int(x + width / 2), int(y - height / 2)),
-                                stroke_color,
-                                stroke,
-                            )
+                    width = result["width"]
+                    height = result["height"]
+                    x = result["x"]
+                    y = result["y"]
 
-                        text_size = cv2.getTextSize(conf, cv2.FONT_HERSHEY_SIMPLEX, 0.4, 1)[0]
-                        cv2.rectangle(
-                                image,
-                                (int(x - width / 2), int(y - height / 2)),
-                                (
-                                    int(x - width / 2 + text_size[0]),
-                                    int(y - height / 2 - text_size[1]),
-                                ),
-                                stroke_color,
-                                -1,
-                            )
-                        cv2.putText(
-                                image,
-                                conf,
-                                (int(x - width / 2), int(y - height / 2)),
-                                cv2.FONT_HERSHEY_SIMPLEX,
-                                0.4,
-                                (255, 255, 255),
-                                thickness=1,
-                            )
+                    cv2.rectangle(
+                            image,
+                            (int(x - width / 2), int(y + height / 2)),
+                            (int(x + width / 2), int(y - height / 2)),
+                            stroke_color,
+                            stroke,
+                        )
 
-                    _, tail = os.path.split(filename)
-                    pre, _ = os.path.splitext(tail)
-                    detected_filename = f"/home/ubuntu/dentasis/detections/detected/{worker}/" + pre + ".jpeg"
-                    print(detected_filename)
-                    os.makedirs(os.path.dirname(detected_filename), exist_ok=True)
-                    PIL_image = toImgPIL(image)
-                    PIL_image.save(detected_filename, quality=70, optimize=True)
-                    confs.sort(reverse=True)
+                    text_size = cv2.getTextSize(conf, cv2.FONT_HERSHEY_SIMPLEX, 0.4, 1)[0]
+                    cv2.rectangle(
+                            image,
+                            (int(x - width / 2), int(y - height / 2)),
+                            (
+                                int(x - width / 2 + text_size[0]),
+                                int(y - height / 2 - text_size[1]),
+                            ),
+                            stroke_color,
+                            -1,
+                        )
+                    cv2.putText(
+                            image,
+                            conf,
+                            (int(x - width / 2), int(y - height / 2)),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.4,
+                            (255, 255, 255),
+                            thickness=1,
+                        )
+                print("confs: ", confs)
 
-                    response["denture_images"].append(
-                        {
-                            "image_url": cloudinary_response["url"],
-                            "confidence": confs,
-                        }
+                _, tail = os.path.split(filename)
+                detected_filename = f"/home/ubuntu/dentasis/detections/detected/{worker}/" + tail
+                os.makedirs(os.path.dirname(detected_filename), exist_ok=True)
+                PIL_image = toImgPIL(image)
+                PIL_image.save(detected_filename)
+                print("created: ", detected_filename)
+                confs.sort(reverse=True)
+
+                with open(detected_filename, "rb") as f:
+                    encoded_image = base64.b64encode(f.read()).decode("UTF-8")
+                    cloudinary_response = uploader.upload(
+                        "data:image/jpeg;base64," + encoded_image
                     )
-            finally:
-                pass
-            #    shutil.rmtree(f"/home/ubuntu/dentasis/detections/detect/{worker}")
-            #    shutil.rmtree(f"/home/ubuntu/dentasis/detections/detected/{worker}")
+                    f.close()
+
+                response["denture_images"].append(
+                    {
+                        "image_url": cloudinary_response["url"],
+                        "confidence": confs,
+                    }
+                )
+            #finally:
+            #    shutil.rmtree(f"/home/ubuntu/dentasis/detections/detect/{worker}/")
+            #    shutil.rmtree(f"/home/ubuntu/dentasis/detections/detected/{worker}/")
             return Response(response, status=status.HTTP_201_CREATED)
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
